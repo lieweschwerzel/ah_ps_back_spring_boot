09005b14f3de1a0d34b03df413efac2bdba83425
lieweschwerzel <36862738+lieweschwerzel@users.noreply.github.com>
2022-07-16 19:58:44 +0200

    cors

--- src/main/java/net/liewe/ah/config/CorsConfiguration.java
+++ src/main/java/net/liewe/ah/config/CorsConfiguration.java
@@ -2,15 +2,16 @@ package net.liewe.ah.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class MyConfiguration implements WebMvcConfigurer {
-
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer
+{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedMethods("*");
+                .allowedMethods("GET", "POST");
     }
-
 }
