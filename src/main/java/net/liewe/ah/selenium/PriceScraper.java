c85d11f5d475f8aa0137a1fce93b437cca688039
lieweschwerzel <36862738+lieweschwerzel@users.noreply.github.com>
2022-07-16 18:16:51 +0200

    Update PriceScraper.java

--- src/main/java/net/liewe/ah/selenium/PriceScraper.java
+++ src/main/java/net/liewe/ah/selenium/PriceScraper.java
@@ -1,7 +1,10 @@
 package net.liewe.ah.selenium;
 
 import net.liewe.ah.model.Product;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
