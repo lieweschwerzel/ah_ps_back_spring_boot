35d47ab1b37d5be91051233d2657add04aba7f97
lieweschwerzel <36862738+lieweschwerzel@users.noreply.github.com>
2022-07-18 23:21:04 +0200

    db

--- src/main/java/net/liewe/ah/config/ProductConfig.java
+++ src/main/java/net/liewe/ah/config/ProductConfig.java
@@ -17,22 +17,22 @@ public class ProductConfig {
     CommandLineRunner commandLineRunner(ProductRepository repository){
         return args -> {
 
-//            List<Product> productList = PriceScraper.getAllProducts();
-//
-//            for (Product el: productList){
-//                System.out.println( el );
-//            }
-//
-//            Product ahKomkommer = new Product(
-//                    "AH Komkommer", 0.55, "300gr", "discounted",
-//                    "https://static.ah.nl/dam/product/AHI_434d50313535333334?revLabel=2&rendition=200x200_JPG_Q85&fileType=binary","nog een www" );
-////                    "melk", 0.55, "1L", "discounted", "www.imageurl"
-////            Product melk = new Product(
-////            );
-//            repository.saveAll(productList);
-//            System.out.println("Stored in DB!");
-//
-//            productList.clear();
+            List<Product> productList = PriceScraper.getAllProducts();
+
+            for (Product el: productList){
+                System.out.println( el );
+            }
+
+            Product ahKomkommer = new Product(
+                    "AH Komkommer", 0.55, "300gr", "discounted",
+                    "https://static.ah.nl/dam/product/AHI_434d50313535333334?revLabel=2&rendition=200x200_JPG_Q85&fileType=binary","nog een www" );
+//                    "melk", 0.55, "1L", "discounted", "www.imageurl"
+//            Product melk = new Product(
+//            );
+            repository.saveAll(productList);
+            System.out.println("Stored in DB!");
+
+            productList.clear();
         };
     }
 }
